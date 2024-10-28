141b037257bb61c2252c4a279c1fdc4d45218980
yite <yite@icloud.com>
2024-10-28 11:16:16 +0800

    generate python data

--- generate_client.sh
+++ generate_client.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
-# Define output folder
-OUTPUT_DIR="fugledata-go"
+# Define output folders
+OUTPUT_DIR_GO="fugledata-go"
+OUTPUT_DIR_PY="fugledata-py"
 
-# Step 1: Create or empty the "fugle-go-client" directory
-if [ -d "$OUTPUT_DIR" ]; then
-    rm -rf "${OUTPUT_DIR:?}/"*
-else
-    mkdir "$OUTPUT_DIR"
-fi
+# Function to generate client code
+generate_client() {
+    local output_dir=$1
+    local language=$2
+    local package_name=$3
+    local repo_id=$4
 
-# Step 2: Generate initial client library from the local OpenAPI server
-openapi-generator-cli generate -i https://postgrest.lynxlinkage.com \
-    -g go \
-    -o "./$OUTPUT_DIR" \
-    --package-name fugledata \
-    --git-host github.com \
-    --git-user-id yitech \
-    --git-repo-id fugledata-go
+    # Step 1: Create or empty the output directory
+    if [ -d "$output_dir" ]; then
+        rm -rf "${output_dir:?}/"*
+    else
+        mkdir "$output_dir"
+    fi
 
-cd "$OUTPUT_DIR" && chmod +x ./git_push.sh && /bin/sh ./git_push.sh yitech fugledata-go "minor update" "github.com"
+    # Step 2: Generate initial client library from the local OpenAPI server
+    openapi-generator-cli generate -i https://postgrest.lynxlinkage.com \
+        -g "$language" \
+        -o "./$output_dir" \
+        --package-name "$package_name" \
+        --git-host github.com \
+        --git-user-id yitech \
+        --git-repo-id "$repo_id"
 
-echo "Client code has been successfully generated and modified in ./$OUTPUT_DIR"
+    cd "$output_dir" && chmod +x ./git_push.sh && /bin/sh ./git_push.sh yitech "$repo_id" "minor update" "github.com"
+    cd ..
+    
+    echo "Client code has been successfully generated and pushed for $language in ./$output_dir"
+}
+
+# Generate Go client
+generate_client "$OUTPUT_DIR_GO" "go" "fugledata" "fugledata-go"
+
+# Generate Python client
+generate_client "$OUTPUT_DIR_PY" "python" "fugledata" "fugledata-py"
